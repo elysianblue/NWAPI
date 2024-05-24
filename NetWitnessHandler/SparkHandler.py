44fa10c5e19edeed918f22d89923509676352fa7
Wes Riley <elysian.blue@gmail.com>
2024-05-24 01:20:04 -0500

    testing executeRestApi

--- NetWitnessHandler/SparkHandler.py
+++ NetWitnessHandler/SparkHandler.py
@@ -79,8 +79,8 @@ class SparkHandler:
                 for j in i['results']['fields']:
                     #rec = { 'group': j['group'], 'type': j['type'], 'value': j['value'] }
                     rec.update({ j['type']: j['value'] })
-                rec_list.append(rec)
-                rec = {}
+                    rec_list.append(rec)
+                    rec = {}
                 #print(rec_list)
                 #res_list.extend(rec_list)
                 #rec_list = []
